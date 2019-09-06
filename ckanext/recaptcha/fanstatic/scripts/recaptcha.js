301ea12c51601c67c6ba9662ab4daffd6678577e
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2019-09-06 13:39:12 +0300

    captcha regeneration

--- ckanext/recaptcha/fanstatic/scripts/recaptcha.js
+++ ckanext/recaptcha/fanstatic/scripts/recaptcha.js
@@ -7,16 +7,26 @@ ckan.module('recaptcha', function($, _) {
       siteKey: null
     },
     initialize: function() {
-      var self = this;
-      window.grecaptcha.ready(function() {
-        window.grecaptcha
-          .execute(self.options.siteKey, { action: self.options.action })
-          .then(self._onGetToken.bind(self));
-      });
+
+      ckan.pubsub.subscribe('recaptcha:regenerate-token', this._generateToken.bind(this));
     },
 
     _onGetToken: function(token) {
       this.el.val(token);
+    },
+    _generateToken: function(cb) {
+      var self = this;
+      window.grecaptcha.ready(function() {
+        window.grecaptcha
+          .execute(self.options.siteKey, { action: self.options.action })
+          .then(self._onGetToken.bind(self))
+          .then(function() {
+            if (cb) {
+              return cb();
+            }
+            return null;
+          });
+      });
     }
   };
 });
