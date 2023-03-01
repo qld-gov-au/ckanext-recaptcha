ad7965000c74bb11358e265e6e26a3f778529427
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-03-01 03:22:30 +0200

    new start

--- ckanext/recaptcha/plugin.py
+++ ckanext/recaptcha/plugin.py
@@ -1,21 +1,16 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-from ckanext.recaptcha.helpers import get_helpers
-
 
 class RecaptchaPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IConfigurer)
+    
 
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'recaptcha')
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("assets", "recaptcha")
 
-    # ITemplateHelpers
-
-    def get_helpers(self):
-        return get_helpers()
+    
